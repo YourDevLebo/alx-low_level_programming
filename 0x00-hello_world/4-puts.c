6d1d304dd661293692ca717cefbf12536d220a6b
YourDevLebo <djlebzana@gmail.com>
2021-08-26 13:37:45 +0200

    added 4-puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * use function puts , followed by new line
+ * not allowed to use printf
+ * should end with the value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
