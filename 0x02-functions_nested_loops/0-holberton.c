d1d3ab1ac7a6c8584be0ef654e940524ac9935b2
YourDevLebo <djlebzana@gmail.com>
2021-08-31 16:15:00 -0400

    fixed task 0 and 8

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-	char *c = "Holberton";
-	int i;
-
-	for (i = 0; i <= c[i]; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
