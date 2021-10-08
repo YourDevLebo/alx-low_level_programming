a61219c07564bcfc69b7f46da76c374808b995fe
YourDevLebo <djlebzana@gmail.com>
2021-10-08 15:22:09 -0400

    added task 0 to 4

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * size_t - checks the size
+ * @h: pointer to list_t
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h);
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nul)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return(s);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+this directory is about linked list:
+
+*Mandatory tasks consists of:
+0. 
+1. 
+2. 
+3. 
+4. 
