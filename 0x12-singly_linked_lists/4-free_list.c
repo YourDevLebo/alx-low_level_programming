95317e751cc8c54f9e261c29babf695da1cf9c3c
YourDevLebo <djlebzana@gmail.com>
2021-10-09 05:21:02 -0400

    fixed task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,12 +4,13 @@
 /**
  * free_list - frees a linked list
  * @head: list_t list to be freed
+ * Return: void
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
 		temp = head->next;
 		free(head->str);
